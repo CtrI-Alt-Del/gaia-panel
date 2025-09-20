f428597337841a64a7d2187fea018a3a25fe718d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-20 12:01:59 -0300

    feat: integrate TelemetryService into RestContext and RestMiddleware for enhanced telemetry support

--- src/app/middlewares/rest-middleware.ts
+++ src/app/middlewares/rest-middleware.ts
@@ -3,7 +3,7 @@ import type { Route } from '../+types/root'
 
 import { AxiosRestClient } from '@/rest/axios/axios-rest-client'
 
-import { MembershipService } from '@/rest/services'
+import { MembershipService, TelemetryService } from '@/rest/services'
 import { restContext } from '../contexts/rest-context'
 
 export const RestMiddleware = async ({ context }: Route.LoaderArgs) => {
@@ -11,8 +11,10 @@ export const RestMiddleware = async ({ context }: Route.LoaderArgs) => {
   restClient.setBaseUrl(ENV.serverAppUrl)
 
   const membershipService = MembershipService(restClient)
+  const telemetryService = TelemetryService(restClient)
 
   context.set(restContext, {
     membershipService,
+    telemetryService,
   })
 }

50a388c9f5449264a7fc8ff5a9baca02ad04a507
0thigs <machinimavlogs@gmail.com>
2025-09-20 22:44:38 -0300

    feat: update routes and add new alarm management components including edit form and alarms page

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,21 +1,16 @@
-import {
-  type RouteConfig,
-  index,
-  route,
-  layout,
-} from "@react-router/dev/routes";
-import { ROUTES } from "../core/constants/ROUTES";
+import { type RouteConfig, index, route, layout } from '@react-router/dev/routes'
+import { ROUTES } from '../core/global/constants/routes'
 
 export default [
-  index("routes/auth/login.tsx"),
-  layout("layouts/AppLayout.tsx", [
-    route(ROUTES.dashboard, "routes/telemetry/dashboard.tsx"),
-    route(ROUTES.stations, "routes/telemetry/stations.tsx"),
-    route(`${ROUTES.stations}/:id`, "routes/telemetry/station.tsx"),
-    route(ROUTES.reports, "routes/telemetry/reports.tsx"),
-    route(ROUTES.configuration, "routes/membership/configuration.tsx"),
-    route(ROUTES.profile, "routes/membership/profile.tsx"),
-    route(ROUTES.parameters, "routes/telemetry/parameters.tsx"),
-    route(ROUTES.alerts, "routes/alerting/alarms.tsx"),
+  index('routes/auth/login.tsx'),
+  layout('layouts/app-layout.tsx', [
+    route(ROUTES.dashboard, 'routes/telemetry/dashboard.tsx'),
+    route(ROUTES.stations, 'routes/telemetry/stations.tsx'),
+    route(ROUTES.reports, 'routes/telemetry/reports.tsx'),
+    route(ROUTES.configuration, 'routes/membership/configuration.tsx'),
+    route(ROUTES.profile, 'routes/membership/profile.tsx'),
+    route(ROUTES.parameters, 'routes/telemetry/parameters.tsx'),
+    route(ROUTES.alerts, 'routes/alerting/alarms-route.tsx'),
+    route(ROUTES.users, 'routes/membership/users-route.tsx'),
   ]),
-] satisfies RouteConfig;
+] satisfies RouteConfig

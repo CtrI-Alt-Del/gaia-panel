ea04e6c988819ac514979c9af1689b8d8a5edf3d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-24 22:15:14 -0300

    feat: enhance authentication and membership handling with new context and middleware integration

--- src/app/middlewares/rest-middleware.ts
+++ src/app/middlewares/rest-middleware.ts
@@ -5,10 +5,14 @@ import { AxiosRestClient } from '@/rest/axios/axios-rest-client'
 
 import { MembershipService, TelemetryService } from '@/rest/services'
 import { restContext } from '../contexts/rest-context'
+import { authContext } from '../contexts/auth-context'
 
 export const RestMiddleware = async ({ context }: Route.LoaderArgs) => {
+  const { accessToken } = context.get(authContext)
+
   const restClient = AxiosRestClient()
   restClient.setBaseUrl(ENV.serverAppUrl)
+  if (accessToken) restClient.setAuthorization(accessToken)
 
   const membershipService = MembershipService(restClient)
   const telemetryService = TelemetryService(restClient)

a2a392bd737eba87099191839f3592ec63ffbaed
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-24 22:10:57 -0300

    feat: integrate AuthMiddleware into routes for enhanced authentication handling

--- src/app/routes/auth/sign-in-route.tsx
+++ src/app/routes/auth/sign-in-route.tsx
@@ -1,3 +1,15 @@
 import { SignInPage } from '@/ui/auth/sign-in'
+import type { Route } from '../../+types/root'
+import { authContext } from '@/app/contexts/auth-context'
+import { redirect } from 'react-router'
+import { ROUTES } from '@/core/global/constants/routes'
+import { AuthMiddleware } from '@/app/middlewares/auth-middleware'
+
+export const middleware = [AuthMiddleware]
+
+export const loader = async ({ context }: Route.LoaderArgs) => {
+  const { userId } = context.get(authContext)
+  if (userId) return redirect(ROUTES.stations)
+}
 
 export default SignInPage
--- src/app/routes/membership/users-route.tsx
+++ src/app/routes/membership/users-route.tsx
@@ -5,6 +5,14 @@ import { AxiosRestClient } from '@/rest/axios/axios-rest-client'
 import { MembershipService } from '@/rest/services/membership-service'
 import { UsersPage } from '@/ui/membership/widgets/pages/users'
 import { ENV } from '@/core/global/constants'
+import { AuthMiddleware } from '@/app/middlewares/auth-middleware'
+import { RestMiddleware } from '@/app/middlewares/rest-middleware'
+import { restContext } from '@/app/contexts/rest-context'
+import { authContext } from '@/app/contexts/auth-context'
+import { redirect } from 'react-router'
+import { ROUTES } from '@/core/global/constants/routes'
+import { MembershipMiddleware } from '@/app/middlewares/membership-middleware'
+import { membershipContext } from '@/app/contexts/membership-context'
 
 export const searchParams = {
   name: parseAsString,
@@ -16,24 +24,36 @@ export const searchParams = {
 
 export const loadSearchParams = createLoader(searchParams)
 
-export const loader = async ({ request }: Route.ActionArgs) => {
-  const { nextCursor, previousCursor, pageSize, name, status } = loadSearchParams(request)
+export const middleware = [AuthMiddleware, RestMiddleware, MembershipMiddleware]
 
-  const restClient = AxiosRestClient()
-  restClient.setBaseUrl(ENV.serverAppUrl)
-  const service = MembershipService(restClient)
-  const response = await service.fetchUsers({
+export const loader = async ({ context, request }: Route.ActionArgs) => {
+  const { nextCursor, previousCursor, pageSize, name, status } = loadSearchParams(request)
+  const { user } = context.get(membershipContext)
+
+  console.log('loader', { user })
+
+  const { userId } = context.get(authContext)
+  const { membershipService } = context.get(restContext)
+  const response = await membershipService.fetchUsers({
     nextCursor,
     previousCursor,
     pageSize: Number(pageSize),
     name: name ?? undefined,
     status: status ?? undefined,
   })
+  if (response.isFailure) response.throwError()
 
-  console.log('response', response)
+  if (userId) {
+    const response = await membershipService.fetchUser(userId)
+    if (response.isFailure) response.throwError()
+    if (response.body.role !== 'owner') return redirect(ROUTES.stations)
+  }
+
+  if (!userId) return redirect(ROUTES.stations)
 
   return {
     users: response.body.items,
+    user,
     nextCursor: response.body.nextCursor,
     previousCursor: response.body.previousCursor,
     pageSize: response.body.pageSize,

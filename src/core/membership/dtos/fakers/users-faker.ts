201d9e4d822e7a4d823f4fa993ffac4345229586
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-24 22:15:34 -0300

    feat: add AccountDto type and enhance UserDto with role property

--- src/core/membership/dtos/fakers/users-faker.ts
+++ src/core/membership/dtos/fakers/users-faker.ts
@@ -7,12 +7,13 @@ export class UsersFaker {
     return {
       name: faker.person.fullName(),
       email: faker.internet.email(),
+      role: faker.helpers.arrayElement(['admin', 'user']),
       isActive: true,
       ...baseDto,
     }
   }
 
   static fakeMany(count: number = 10, baseDto?: Partial<UserDto>): UserDto[] {
-    return Array.from({ length: count }, () => this.fakeDto(baseDto))
+    return Array.from({ length: count }, () => UsersFaker.fakeDto(baseDto))
   }
 }

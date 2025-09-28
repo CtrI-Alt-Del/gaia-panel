36f5f9e7a1645b1a4409a251b741b2d3a04037b8
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-28 19:31:22 -0300

    feat: added update alarm in the service

--- src/core/alerting/interfaces/alerting-service.ts
+++ src/core/alerting/interfaces/alerting-service.ts
@@ -4,4 +4,5 @@ import type { AlarmListingParams } from "../types";
 
 export interface AlertingService{
     fetchAlarms(params: AlarmListingParams): Promise<RestResponse<PaginationResponse<AlarmDto>>>
+    updateAlarm(alarm: AlarmDto): Promise<RestResponse<AlarmDto>>
 }

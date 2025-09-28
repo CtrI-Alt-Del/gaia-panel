e5da3a36f4fb69187940b53befa0ec1668e838ec
João Pedro Carvalho <joaopcarvalho.cds@gmail.com>
2025-09-28 12:08:36 -0300

    merge: pull request #28

--- src/core/global/constants/env.ts
+++ src/core/global/constants/env.ts
@@ -1,12 +1,11 @@
 import z from 'zod'
 
 const envSchema = z.object({
-  serverAppUrl: z.url(),
-  clerkSecretKey: z.string(),
+  gaiaServerUrl: z.url(),
 })
 
-
 export const ENV = envSchema.parse({
-  serverAppUrl: import.meta.env.VITE_SERVER_APP_URL || 'http://localhost:3000',
-  clerkSecretKey: import.meta.env.VITE_CLERK_SECRET_KEY || '',
+  gaiaServerUrl:
+    import.meta.env.VITE_GAIA_SERVER_URL ||
+    'http://dev-gaia-server-sd.dev.gaia.local:3333',
 })

3dc2fd02b5af30650b222a942ae17167159d487c
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-28 17:38:09 -0300

    feat: added alertingService in use rest

--- src/ui/global/hooks/use-rest.ts
+++ src/ui/global/hooks/use-rest.ts
@@ -1,6 +1,7 @@
 import { ENV } from '@/core/global/constants'
 import { AxiosRestClient } from '@/rest/axios/axios-rest-client'
 import { MembershipService, TelemetryService } from '@/rest/services'
+import { AlertingService } from '@/rest/services/alerting-service'
 
 export function useRest() {
   const restClient = AxiosRestClient()
@@ -10,7 +11,7 @@ export function useRest() {
   return {
     membershipService: MembershipService(restClient),
     telemetryService: TelemetryService(restClient),
-    alertingService: null,
+    alertingService: AlertingService(restClient),
     authService: null,
   }
 }

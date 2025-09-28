beef10769c7d2cf1446624d5c0cdb7da74be1513
Caique Péricles Silva <110677265+PasteldePaodeCoxinha@users.noreply.github.com>
2025-09-28 15:09:56 -0300

    merge: #29
    * feat: made type alarm listing params
    
    * feat: created alerting service interface
    
    * feat: created alerting service in services
    
    * refactor: made that alarms-route loader fetch database data
    
    * feat: changed alarm routing to get alarms

--- src/core/telemetry/interfaces/telemetry-service.ts
+++ src/core/telemetry/interfaces/telemetry-service.ts
@@ -1,9 +1,7 @@
-import type { PaginationResponse, RestResponse } from '@/core/global/responses'
-import type { AlarmDto } from '../../alerting/dtos/alarm-dto'
+import type {RestResponse } from '@/core/global/responses'
 import type { ParameterDto } from '../dtos/parameter-dto'
 
 export interface TelemetryService {
-  fetchAlarms(): Promise<RestResponse<PaginationResponse<AlarmDto>>>
   fetchParameters(): Promise<RestResponse<ParameterDto[]>>
   createParameter(parameter: ParameterDto): Promise<RestResponse<ParameterDto>>
   updateParameter(parameter: ParameterDto): Promise<RestResponse<ParameterDto>>

1e4733bc3ba765763bbb87e8375f44233c0ca89a
Tico1606 <gabrielsoliveira1606@gmail.com>
2025-10-08 08:46:13 -0300

    feat: GAIA-145 add AlertsService and AlertDto; implement alerts routes and faker utility

---
+++ src/rest/services/alerts-service.ts
@@ -0,0 +1,22 @@
+import type { RestClient } from '@/core/global/interfaces'
+import type { AlertsService as IAlertsService } from '@/core/alerts/interfaces/alerts-service'
+import type { AlertListingParams } from '@/core/alerts/types'
+
+export const AlertsService = (restClient: RestClient): IAlertsService => {
+  return {
+    async fetchAlerts(params: AlertListingParams) {
+      if (params.date) restClient.setQueryParam('date', params.date)
+      if (params.level) restClient.setQueryParam('level', params.level)
+      if (params.nextCursor) restClient.setQueryParam('nextCursor', params.nextCursor)
+      if (params.previousCursor)
+        restClient.setQueryParam('previousCursor', params.previousCursor)
+      if (params.pageSize)
+        restClient.setQueryParam('pageSize', params.pageSize.toString())
+      return await restClient.get('/alerts')
+    },
+
+    async fetchAlert(alertId: string) {
+      return await restClient.get(`/alerts/${alertId}`)
+    },
+  }
+}

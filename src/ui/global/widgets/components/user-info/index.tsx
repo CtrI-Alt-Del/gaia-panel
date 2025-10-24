6ead12256748e52e38ae269dba92532afcb1caf2
Tico1606 <gabrielsoliveira1606@gmail.com>
2025-10-24 17:26:52 -0300

    feat: implement visitor information display and update sidebar logic for user roles

--- src/ui/global/widgets/components/user-info/index.tsx
+++ src/ui/global/widgets/components/user-info/index.tsx
@@ -1,8 +1,16 @@
 import { useLoaderData } from 'react-router'
 import { UserInfoView } from './user-info-view'
 import type { loader } from '@/app/layouts/app-layout'
+import { VisitorInfoView } from './visitor-info-view'
 
 export const UserInfo = () => {
-  const { user } = useLoaderData<typeof loader>()
-  if (user) return <UserInfoView name={user.name} email={user.email} />
+  const { user, isVisitor } = useLoaderData<typeof loader>()
+
+  if (isVisitor) {
+    return <VisitorInfoView isVisitor={isVisitor} />
+  }
+
+  if (user) {
+    return <UserInfoView name={user.name} email={user.email} />
+  }
 }

78451944f803dbdde7e28efce074d8ef3fce81d9
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-25 11:30:40 -0300

    feat: add health check for gaia server

--- src/app/routes/auth/sign-in-route.tsx
+++ src/app/routes/auth/sign-in-route.tsx
@@ -1,3 +1,18 @@
+import { ENV } from '@/core/global/constants'
 import { SignInPage } from '@/ui/auth/sign-in'
+
+export const loader = async () => {
+  try {
+    const response = await fetch(ENV.gaiaServerUrl)
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch health check')
+    }
+    console.log(await response.json())
+  } catch (error) {
+    console.log('Failed to connect to server')
+    console.log('error', error)
+  }
+}
 
 export default SignInPage

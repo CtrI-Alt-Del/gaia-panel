8b9ca5cd3b9c5005361c840a691cbe4ad91808c7
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-28 18:05:22 -0300

    fix: staging Deployment Workflow

--- src/core/global/constants/client-env.ts
+++ src/core/global/constants/client-env.ts
@@ -1,5 +1,7 @@
 import { envSchema } from '@/validation/global'
 
+console.log('CLIENT ENV', import.meta.env)
+
 export const CLIENT_ENV = envSchema.parse({
   gaiaServerUrl: import.meta.env.VITE_GAIA_SERVER_URL,
 })

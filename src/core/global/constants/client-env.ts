ee1b542e07540172ae560c480d5d17e2e054b813
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-09 16:10:48 -0300

    fix: #GAIA-166 log parsed CLIENT_ENV in client-env.ts for better debugging

--- src/core/global/constants/client-env.ts
+++ src/core/global/constants/client-env.ts
@@ -1,9 +1,10 @@
 import { envSchema } from '@/validation/global'
 
-console.log('CLIENT ENV', import.meta.env)
 
 export const CLIENT_ENV = envSchema.parse({
   gaiaServerUrl:
     import.meta.env.VITE_GAIA_SERVER_URL ??
     'http://dev-gaia-server-sd.dev.gaia.local:3333',
 })
+
+console.log('CLIENT ENV', CLIENT_ENV)

f6d51fee66f7be64e2b86c1ae5e6121002402f86
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-09 20:50:31 -0300

    fix: #GAIA-166 update CLIENT_ENV to use production server URL for better deployment configuration

--- src/core/global/constants/client-env.ts
+++ src/core/global/constants/client-env.ts
@@ -4,7 +4,7 @@ import { envSchema } from '@/validation/global'
 export const CLIENT_ENV = envSchema.parse({
   gaiaServerUrl:
     import.meta.env.VITE_GAIA_SERVER_URL ??
-    'http://dev-gaia-server-sd.dev.gaia.local:3333',
+    'dev-alb-2035488909.us-east-1.elb.amazonaws.com/server', 
 })
 
 console.log('CLIENT ENV', CLIENT_ENV)

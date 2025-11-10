6042d14b9b3a123de903946e0e43c80aed41a37b
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-09 21:19:17 -0300

    fix: #GAIA-166 update CLIENT_ENV to use HTTP protocol for gaiaServerUrl

--- src/core/global/constants/client-env.ts
+++ src/core/global/constants/client-env.ts
@@ -4,7 +4,7 @@ import { envSchema } from '@/validation/global'
 export const CLIENT_ENV = envSchema.parse({
   gaiaServerUrl:
     import.meta.env.VITE_GAIA_SERVER_URL ??
-    'dev-alb-2035488909.us-east-1.elb.amazonaws.com/server', 
+    'http://dev-alb-2035488909.us-east-1.elb.amazonaws.com/server', 
 })
 
 console.log('CLIENT ENV', CLIENT_ENV)

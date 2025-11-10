d5f88ec15b9d35e337afc5e69f6bd034485a65cf
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-10 06:32:02 -0300

    feat: #GAIA-166 enhance load testing script with additional stages for varying traffic levels

--- test/load-test.ts
+++ test/load-test.ts
@@ -5,7 +5,19 @@ const GAIA_PANEL_URL = 'http://dev-alb-2035488909.us-east-1.elb.amazonaws.com'
 
 export const options = {
   stages: [
-    { duration: '30s', target: 10 },
+    { duration: '60s', target: 80 },    // sobe até 80 (abaixo do normal)
+    { duration: '120s', target: 80 },   // segura em 80
+
+    { duration: '60s', target: 160 },   // sobe até 160 (carga normal)
+    { duration: '120s', target: 160 },  // segura em 160
+
+    { duration: '60s', target: 240 },   // sobe até 240 (perto do limite)
+    { duration: '120s', target: 240 },  // segura em 240
+
+    { duration: '60s', target: 320 },   // sobe até 320 (pico)
+    { duration: '120s', target: 320 },  // segura em 320
+
+    { duration: '180s', target: 0 },    // re
   ],
   thresholds: {
     http_req_duration: ['p(95)<2000'], // 95% of requests must complete below 2s

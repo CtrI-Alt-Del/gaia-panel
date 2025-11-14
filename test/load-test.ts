ba6a989fdbd449091f26aa693d429c5123d729f5
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-13 21:07:50 -0300

    refactor: #GAIA-166 clean up load test

--- test/load-test.ts
+++ test/load-test.ts
@@ -1,24 +1,25 @@
 import http from 'k6/http'
 import { check, sleep } from 'k6'
+// @ts-expect-error
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js'
 
 const GAIA_PANEL_URL = 'http://dev-alb-2035488909.us-east-1.elb.amazonaws.com'
 
 export const options = {
   stages: [
-    { duration: '60s', target: 80 },    // sobe até 80 (abaixo do normal)
-    { duration: '120s', target: 80 },   // segura em 80
+    { duration: '60s', target: 80 }, // sobe até 80 (abaixo do normal)
+    { duration: '120s', target: 80 }, // segura em 80
 
-    { duration: '60s', target: 160 },   // sobe até 160 (carga normal)
-    { duration: '120s', target: 160 },  // segura em 160
+    { duration: '60s', target: 160 }, // sobe até 160 (carga normal)
+    { duration: '120s', target: 160 }, // segura em 160
 
-    { duration: '60s', target: 240 },   // sobe até 240 (perto do limite)
-    { duration: '120s', target: 240 },  // segura em 240
+    { duration: '60s', target: 240 }, // sobe até 240 (perto do limite)
+    { duration: '120s', target: 240 }, // segura em 240
 
-    { duration: '60s', target: 320 },   // sobe até 320 (pico)
-    { duration: '120s', target: 320 },  // segura em 320
+    { duration: '60s', target: 320 }, // sobe até 320 (pico)
+    { duration: '120s', target: 320 }, // segura em 320
 
-    { duration: '180s', target: 0 },    // re
+    { duration: '180s', target: 0 }, // re
   ],
   thresholds: {
     http_req_duration: ['p(95)<2000', 'p(99)<1000'],
@@ -28,7 +29,7 @@ export const options = {
 
 export function handleSummary(data: any) {
   return {
-    "test-load-report.html": htmlReport(data),
+    'test-load-report.html': htmlReport(data),
   }
 }
 

0892125a14c6d67dd9bb5178e749619cea662c80
Gabriel Oliveira <118027049+Tico1606@users.noreply.github.com>
2025-11-21 18:08:48 -0300

    merge: #72
    * feat: implement pdf to services
    
    * feat: add station report download button
    
    * fix: update import path for station report download component
    
    * fix: update sign-in page text for clarity

--- src/core/global/interfaces/rest-client.ts
+++ src/core/global/interfaces/rest-client.ts
@@ -1,11 +1,31 @@
 import type { RestResponse } from '../responses/rest-response'
 
+export type RestClientRequestConfig = {
+  responseType?: 'json' | 'blob' | 'arraybuffer'
+  headers?: Record<string, string>
+}
+
 export interface RestClient {
-  get<ResponseBody>(url: string): Promise<RestResponse<ResponseBody>>
-  post<ResponseBody>(url: string, body?: unknown): Promise<RestResponse<ResponseBody>>
-  patch<ResponseBody>(url: string, body?: unknown): Promise<RestResponse<ResponseBody>>
-  put<ResponseBody>(url: string, body?: unknown): Promise<RestResponse<ResponseBody>>
-  delete(url: string): Promise<RestResponse>
+  get<ResponseBody>(
+    url: string,
+    config?: RestClientRequestConfig,
+  ): Promise<RestResponse<ResponseBody>>
+  post<ResponseBody>(
+    url: string,
+    body?: unknown,
+    config?: RestClientRequestConfig,
+  ): Promise<RestResponse<ResponseBody>>
+  patch<ResponseBody>(
+    url: string,
+    body?: unknown,
+    config?: RestClientRequestConfig,
+  ): Promise<RestResponse<ResponseBody>>
+  put<ResponseBody>(
+    url: string,
+    body?: unknown,
+    config?: RestClientRequestConfig,
+  ): Promise<RestResponse<ResponseBody>>
+  delete(url: string, config?: RestClientRequestConfig): Promise<RestResponse>
   setBaseUrl(url: string): void
   setHeader(key: string, value: string): void
   setAuthorization(token: string): void

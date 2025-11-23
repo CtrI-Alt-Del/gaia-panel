d92638a5a40be357839a433784ae9d27a57b1ebc
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-11-23 12:52:31 -0300

    fix: #GAIA-207 update default Gaia server URL in client environment configuration

--- src/core/global/constants/client-env.ts
+++ src/core/global/constants/client-env.ts
@@ -1,10 +1,9 @@
 import { envSchema } from '@/validation/global'
 
-
 export const CLIENT_ENV = envSchema.parse({
   gaiaServerUrl:
     import.meta.env.VITE_GAIA_SERVER_URL ??
-    'http://dev-alb-2035488909.us-east-1.elb.amazonaws.com/server', 
+    'http://dev-alb-1220408087.us-east-1.elb.amazonaws.com/server',
 })
 
 console.log('CLIENT ENV', CLIENT_ENV)
